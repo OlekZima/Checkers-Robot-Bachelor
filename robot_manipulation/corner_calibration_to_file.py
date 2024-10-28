8174ae78121e7ed7d0bce1000a43828941d9211d
OlekZima <zimenkoalex34@gmail.com>
2024-10-28 20:44:39 +0100

    Refactor the corner_calibration_to_file.py

--- robot_manipulation/corner_calibration_to_file.py
+++ robot_manipulation/corner_calibration_to_file.py
@@ -1,53 +1,44 @@
+import os
+
 from serial.tools import list_ports
 from pydobotplus import Dobot
 import numpy as np
 import cv2 as cv
+import sys
+import termios
+
+
+def flush_input():
+    termios.tcflush(sys.stdin, termios.TCIFLUSH)
 
 
 class DobotController:
 
     @classmethod
     def get_xy_from_id(cls, id):
-        y = int((id - 1) / 4)
-
+        y = (id - 1) // 4
         x = ((id - 1) % 4) * 2
+
         if y % 2 == 0:
             x += 1
 
         return x, y
 
     def __init__(self):
-
         # Connecting to DOBOT
         available_ports = list_ports.comports()
-
-        print("\nPlease select port by index")
-        for i, p in enumerate(available_ports):
-            print(f"[{i}]: {p}")
-
-        port_idx = int(input())
-
-        port = available_ports[port_idx].device
-
-        self.device = Dobot(port=port)
-
-        x, y, z, r = self.device.get_pose().position
-        print(f"\nx:{x} y:{y} z:{z} r:{r}")
-
-        self.device.suck(False)
-
-        # Calibrating for board
+        self.device = self._connect_to_dobot(available_ports)
 
         # Board field numerating convention:
         #  upper_left = [0][0]
         #  upper_right = [7][0]
         #  bottom_left = [0][7]
         #  bottom_right = [7][7]
-        self.board = np.zeros(shape=(8, 8, 3), dtype=float)
-        self.side_pockets = np.zeros(shape=(2, 4, 3), dtype=float)
-        self.dispose_area = np.zeros(shape=(3), dtype=float)
-        self.home_pos = np.zeros(shape=(3), dtype=float)
-        self.kings_available = 8
+        self.board = np.zeros((8, 8, 3), dtype=float)
+        self.side_pockets = np.zeros((2, 4, 3), dtype=float)
+        self.dispose_area = np.zeros(3, dtype=float)
+        self.home_pos = np.zeros(3, dtype=float)
+        # self.kings_available = 8
 
         self.default_calibration_positions = [
             [244, 73, -9],  # upper left board corner
@@ -66,8 +57,17 @@ class DobotController:
 
         print("Controller created")
 
-    def keyboard_move_dobot(self, increment=1.0):
+    @staticmethod
+    def _connect_to_dobot(available_ports):
+        print("\nPlease select port by index")
+        for i, p in enumerate(available_ports):
+            print(f"[{i}]: {p}")
 
+        port_idx = int(input())
+        port = available_ports[port_idx].device
+        return Dobot(port=port)
+
+    def keyboard_move_dobot(self, increment=1.0):
         x, y, z, _ = self.device.get_pose().position
 
         instruction_frame = np.zeros(
@@ -102,258 +102,46 @@ class DobotController:
         cv.destroyAllWindows()
 
     def calibrate(self, height=10):
+        def calibrate_point(index, storage_array, storage_indices, message):
+            print(message)
+            default_pos = self.default_calibration_positions[index]
+            # Move to default position plus height
+            self.move_arm(default_pos[0], default_pos[1], default_pos[2] + height, wait=True)
+            # Move to default position
+            self.move_arm(default_pos[0], default_pos[1], default_pos[2], wait=True)
+            # Allow user to fine-tune the position
+            self.keyboard_move_dobot()
+            # Get current position
+            x, y, z, _ = self.device.get_pose().position
+            # Store the position
+            storage = storage_array
+            if storage_indices:
+                for idx in storage_indices[:-1]:
+                    storage = storage[idx]
+                storage_idx = storage_indices[-1]
+                storage[storage_idx][:] = [x, y, z]
+            else:
+                storage[:] = [x, y, z]
+            print(f"x = {x}\ty = {y}\tz = {z}")
+            # Move the arm back up
+            self.move_arm(x, y, z + height, wait=True)
 
-        # Gathering info for 8x8 board
-        print(
-            "Please place the dobot tool on upper left board corner (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[0][0],
-            self.default_calibration_positions[0][1],
-            self.default_calibration_positions[0][2] + height,
-            wait=True,
-        )
+        calibration_points = [
+            (0, self.board, [0, 0], "upper left board corner"),
+            (1, self.board, [7, 0], "upper right board corner"),
+            (2, self.board, [0, 7], "bottom left board corner"),
+            (3, self.board, [7, 7], "bottom right board corner"),
+            (4, self.side_pockets, [0, 0], "upper left side pocket"),
+            (5, self.side_pockets, [0, 3], "bottom left side pocket"),
+            (6, self.side_pockets, [1, 0], "upper right side pocket"),
+            (7, self.side_pockets, [1, 3], "bottom right side pocket"),
+            (8, self.dispose_area, [], "dispose area"),
+            (9, self.home_pos, [], "default/home position"),
+        ]
 
-        self.move_arm(
-            self.default_calibration_positions[0][0],
-            self.default_calibration_positions[0][1],
-            self.default_calibration_positions[0][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.board[0][0][0] = x
-        self.board[0][0][1] = y
-        self.board[0][0][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        print(
-            "Please place the dobot tool on upper right board corner (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[1][0],
-            self.default_calibration_positions[1][1],
-            self.default_calibration_positions[1][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[1][0],
-            self.default_calibration_positions[1][1],
-            self.default_calibration_positions[1][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.board[7][0][0] = x
-        self.board[7][0][1] = y
-        self.board[7][0][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        print(
-            "Please place the dobot tool on bottom left board corner (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[2][0],
-            self.default_calibration_positions[2][1],
-            self.default_calibration_positions[2][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[2][0],
-            self.default_calibration_positions[2][1],
-            self.default_calibration_positions[2][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.board[0][7][0] = x
-        self.board[0][7][1] = y
-        self.board[0][7][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        print(
-            "Please place the dobot tool on bottom right board corner (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[3][0],
-            self.default_calibration_positions[3][1],
-            self.default_calibration_positions[3][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[3][0],
-            self.default_calibration_positions[3][1],
-            self.default_calibration_positions[3][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.board[7][7][0] = x
-        self.board[7][7][1] = y
-        self.board[7][7][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        # Gathering info for side pockets
-        print(
-            "Please place the dobot tool on upper left side pocket (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[4][0],
-            self.default_calibration_positions[4][1],
-            self.default_calibration_positions[4][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[4][0],
-            self.default_calibration_positions[4][1],
-            self.default_calibration_positions[4][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.side_pockets[0][0][0] = x
-        self.side_pockets[0][0][1] = y
-        self.side_pockets[0][0][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        print(
-            "Please place the dobot tool on bottom left side pocket (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[5][0],
-            self.default_calibration_positions[5][1],
-            self.default_calibration_positions[5][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[5][0],
-            self.default_calibration_positions[5][1],
-            self.default_calibration_positions[5][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.side_pockets[0][3][0] = x
-        self.side_pockets[0][3][1] = y
-        self.side_pockets[0][3][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        print(
-            "Please place the dobot tool on upper right side pocket (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[6][0],
-            self.default_calibration_positions[6][1],
-            self.default_calibration_positions[6][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[6][0],
-            self.default_calibration_positions[6][1],
-            self.default_calibration_positions[6][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.side_pockets[1][0][0] = x
-        self.side_pockets[1][0][1] = y
-        self.side_pockets[1][0][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        print(
-            "Please place the dobot tool on bottom right side pocket (from its perspective) and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[7][0],
-            self.default_calibration_positions[7][1],
-            self.default_calibration_positions[7][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[7][0],
-            self.default_calibration_positions[7][1],
-            self.default_calibration_positions[7][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.side_pockets[1][3][0] = x
-        self.side_pockets[1][3][1] = y
-        self.side_pockets[1][3][2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-        self.move_arm(x, y, z + height, wait=True)
-
-        # Gathering info for dispose area
-        print(
-            "Please place the dobot tool where you would like it to dispose of pieces and press enter"
-        )
-        self.move_arm(
-            self.default_calibration_positions[8][0],
-            self.default_calibration_positions[8][1],
-            self.default_calibration_positions[8][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[8][0],
-            self.default_calibration_positions[8][1],
-            self.default_calibration_positions[8][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.dispose_area[0] = x
-        self.dispose_area[1] = y
-        self.dispose_area[2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
-
-        # Gathering info for home pos
-        print(
-            "Please place the dobot tool where you would like it to have a home/default position"
-        )
-        self.move_arm(
-            self.default_calibration_positions[9][0],
-            self.default_calibration_positions[9][1],
-            self.default_calibration_positions[9][2] + height,
-            wait=True,
-        )
-
-        self.move_arm(
-            self.default_calibration_positions[9][0],
-            self.default_calibration_positions[9][1],
-            self.default_calibration_positions[9][2],
-            wait=True,
-        )
-        self.keyboard_move_dobot()
-        x, y, z, _ = self.device.get_pose().position
-        self.home_pos[0] = x
-        self.home_pos[1] = y
-        self.home_pos[2] = z
-
-        print(f"x = {x}\ty = {y}\tz = {z}")
+        for idx, storage, indices, description in calibration_points:
+            message = f"Please place the DOBOT on {description} (from its perspective)"
+            calibrate_point(idx, storage, indices, message)
 
         # Calculating all fields
 
@@ -366,123 +154,61 @@ class DobotController:
         except Exception as e:
             print(e)
 
+    @staticmethod
+    def linear_interpolate(a, b, t):
+        return a + t * (b - a)
+
     def interpolate_board_fields(self):
 
         # 1) Interpolating border fields coordinates
         for i in range(1, 7):
-
-            # 1.1) Left column (x = 0)
+            t = i / 7.0
             for k in range(3):
-                self.board[0][i][k] = (
-                    self.board[0][0][k]
-                    + i * (self.board[0][7][k] - self.board[0][0][k]) / 7.0
-                )
-
-            # 1.2) Right column (x = 7)
-            for k in range(3):
-                self.board[7][i][k] = (
-                    self.board[7][0][k]
-                    + i * (self.board[7][7][k] - self.board[7][0][k]) / 7.0
-                )
-
-            # 1.3 Upper row (y = 0)
-            for k in range(3):
-                self.board[i][0][k] = (
-                    self.board[0][0][k]
-                    + i * (self.board[7][0][k] - self.board[0][0][k]) / 7.0
-                )
-
-            # 1.4 Bottom row (y = 7)
-            for k in range(3):
-                self.board[i][7][k] = (
-                    self.board[0][7][k]
-                    + i * (self.board[7][7][k] - self.board[0][7][k]) / 7.0
-                )
+                # 1.1) Left column (x = 0)
+                self.board[0][i][k] = self.linear_interpolate(self.board[0][0][k], self.board[0][7][k], t)
+                # 1.2) Right column (x = 7)
+                self.board[7][i][k] = self.linear_interpolate(self.board[7][0][k], self.board[7][7][k], t)
+                # 1.3 Upper row (y = 0)
+                self.board[i][0][k] = self.linear_interpolate(self.board[0][0][k], self.board[7][0][k], t)
+                # Bottom row (y = 7)
+                self.board[i][7][k] = self.linear_interpolate(self.board[0][7][k], self.board[7][7][k], t)
 
         # 2) Interpolating inner points
         for x in range(1, 7):
+            t_x = x / 7.0
             for y in range(1, 7):
-                for z in range(1, 3):
-                    self.board[x][y][z] = (
-                        (
-                            self.board[0][y][z]
-                            + x * (self.board[7][y][z] - self.board[0][y][z]) / 7.0
-                        )
-                        + (
-                            self.board[x][0][z]
-                            + y * (self.board[x][7][z] - self.board[x][0][z]) / 7.0
-                        )
-                    ) / 2.0
+                t_y = y / 7.0
+                for z in range(3):
+                    self.board[x][y][z] = (self.linear_interpolate(self.board[0][y][z], self.board[7][y][z], t_x) +
+                                           self.linear_interpolate(self.board[x][0][z], self.board[x][7][z], t_y)
+                                           / 2.0)
 
     def interpolate_side_pockets(self):
-        for k in range(2):
-            self.side_pockets[0][1][k] = (
-                self.side_pockets[0][0][k] * 2 + self.side_pockets[0][3][k]
-            ) / 3.0
+        for k in range(3):
+            self.side_pockets[0][1][k] = (self.side_pockets[0][0][k] * 2 + self.side_pockets[0][3][k]) / 3.0
+            self.side_pockets[1][1][k] = (self.side_pockets[1][0][k] * 2 + self.side_pockets[1][3][k]) / 3.0
 
-        for k in range(2):
-            self.side_pockets[0][2][k] = (
-                self.side_pockets[0][0][k] + self.side_pockets[0][3][k] * 2
-            ) / 3.0
-
-        for k in range(2):
-            self.side_pockets[1][1][k] = (
-                self.side_pockets[1][0][k] * 2 + self.side_pockets[1][3][k]
-            ) / 3.0
-
-        for k in range(2):
-            self.side_pockets[1][2][k] = (
-                self.side_pockets[1][0][k] + self.side_pockets[1][3][k] * 2
-            ) / 3.0
+            self.side_pockets[0][2][k] = (self.side_pockets[0][0][k] + self.side_pockets[0][3][k] * 2) / 3.0
+            self.side_pockets[1][2][k] = (self.side_pockets[1][0][k] + self.side_pockets[1][3][k] * 2) / 3.0
 
 
 if __name__ == "__main__":
     dobot = DobotController()
-
     print("\nPut name of the file you would like to save configuration in:")
-
+    flush_input()
     filename = input()
 
-    f = open("robot_manipulation/configuration_files/" + filename, "w+")
+    directory = "robot_manipulation/configuration_files/"
+    os.makedirs(directory, exist_ok=True)
 
-    for i in range(1, 33, 1):
-        x, y = DobotController.get_xy_from_id(i)
-        f.write(
-            str(dobot.board[x][y][0])
-            + ";"
-            + str(dobot.board[x][y][1])
-            + ";"
-            + str(dobot.board[x][y][2])
-            + "\n"
-        )
+    with open(os.path.join(directory, filename), mode="w") as f:
+        for i in range(1, 33):
+            x, y = DobotController.get_xy_from_id(i)
+            f.write(f"{dobot.board[x][y][0]};{dobot.board[x][y][1]};{dobot.board[x][y][2]}\n")
 
-    for i in range(0, 2, 1):
-        for j in range(0, 4, 1):
-            f.write(
-                str(dobot.side_pockets[i][j][0])
-                + ";"
-                + str(dobot.side_pockets[i][j][1])
-                + ";"
-                + str(dobot.side_pockets[i][j][2])
-                + "\n"
-            )
+        for i in range(2):
+            for j in range(4):
+                f.write(f"{dobot.side_pockets[i][j][0]};{dobot.side_pockets[i][j][1]};{dobot.side_pockets[i][j][2]}\n")
 
-    f.write(
-        str(dobot.dispose_area[0])
-        + ";"
-        + str(dobot.dispose_area[1])
-        + ";"
-        + str(dobot.dispose_area[2])
-        + "\n"
-    )
-
-    f.write(
-        str(dobot.home_pos[0])
-        + ";"
-        + str(dobot.home_pos[1])
-        + ";"
-        + str(dobot.home_pos[2])
-        + "\n"
-    )
-
-    f.close()
+        f.write(f"{dobot.dispose_area[0]};{dobot.dispose_area[1]};{dobot.dispose_area[2]}\n")
+        f.write(f"{dobot.home_pos[0]};{dobot.home_pos[1]};{dobot.home_pos[2]}\n")
