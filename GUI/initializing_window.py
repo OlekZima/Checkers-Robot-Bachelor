6ad3bb47581344ef9e4d21e7056af0f4f00b0819
OlekZima <zimenkoalex34@gmail.com>
2024-10-31 13:55:28 +0100

    Delete unused import

--- GUI/initializing_window.py
+++ GUI/initializing_window.py
@@ -2,8 +2,7 @@ import customtkinter as ctk
 from customtkinter import CTkImage
 from serial.tools import list_ports
 import cv2
-from PIL import Image, ImageTk
-
+from PIL import Image
 from computer_vision.gameplay_recognition import list_ports as list_camera_ports
 
 ctk.set_appearance_mode("system")
