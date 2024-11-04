a8788b5180bba644d3dcec60ad59afd60d25af56
OlekZima <zimenkoalex34@gmail.com>
2024-11-04 13:40:40 +0100

    Format refactor

--- computer_vision/checkers_recognition/checkers_recognition.py
+++ computer_vision/checkers_recognition/checkers_recognition.py
@@ -1,51 +1,56 @@
 import cv2 as cv
 import numpy as np
 import math
-import traceback
 from enum import Enum
 
-def get_pts_dist(pt1 = [0,0], pt2 = [0,0]):
+
+def get_pts_dist(pt1=[0, 0], pt2=[0, 0]):
     dx = pt1[0] - pt2[0]
     dy = pt1[1] - pt2[1]
 
-    dx = float(dx*dx)
-    dy = float(dy*dy)
+    # dx = float(dx*dx)
+    # dy = float(dy*dy)
 
-    return math.sqrt(dx+dy)
+    # return math.sqrt(dx+dy)
+    return math.hypot(dx, dy)
 
-def get_avg_pos(pts = [[0,0],[0,0]]):
+
+def get_avg_pos(pts=[[0, 0], [0, 0]]):
     x_avg, y_avg = 0, 0
 
     for pt in pts:
         x_avg += pt[0]
         y_avg += pt[1]
 
-    x_avg = int( float(x_avg) / float(len(pts)) )
-    y_avg = int( float(y_avg) / float(len(pts)) )
+    x_avg = int(float(x_avg) / float(len(pts)))
+    y_avg = int(float(y_avg) / float(len(pts)))
 
     return [x_avg, y_avg]
 
-def get_img_mask(pts, img_shape, margin = 10):
+
+def get_img_mask(pts, img_shape, margin=10):
     center = get_avg_pos(pts)
 
     for pt in pts:
         dst = get_pts_dist(pt, center)
-        pt[0] = center[0] + int(float(pt[0]-center[0])/dst*(dst+margin))
-        pt[1] = center[1] + int(float(pt[1]-center[1])/dst*(dst+margin))
-    
+        pt[0] = center[0] + int(float(pt[0] - center[0]) / dst * (dst + margin))
+        pt[1] = center[1] + int(float(pt[1] - center[1]) / dst * (dst + margin))
+
     pts = np.array(pts)
-    mask = np.zeros(img_shape[:2],dtype="uint8")
+    mask = np.zeros(img_shape[:2], dtype="uint8")
     cv.fillConvexPoly(mask, pts, 1)
     return mask
 
+
 def distance_from_color(bgr_sample, bgr_target):
     dist = (
-        (bgr_sample[0]-bgr_target[0])**2 + 
-        (bgr_sample[1]-bgr_target[1])**2 + 
-        (bgr_sample[2]-bgr_target[2])**2
+        (bgr_sample[0] - bgr_target[0]) ** 2
+        + (bgr_sample[1] - bgr_target[1]) ** 2
+        + (bgr_sample[2] - bgr_target[2]) ** 2
     )
     dist = math.sqrt(dist)
     return dist
+
 
 def get_avg_color(img):
     b_val = 0
@@ -58,7 +63,7 @@ def get_avg_color(img):
             g_val += j[1]
             r_val += j[2]
 
-    n = img.shape[0]*img.shape[1]
+    n = img.shape[0] * img.shape[1]
     b_val /= n
     g_val /= n
     r_val /= n
@@ -70,52 +75,63 @@ class Color(Enum):
     RED = 1
     GREEN = 2
 
+
 class Checkers:
 
-    checkers = [] #storing all checkers
+    checkers = []  # storing all checkers
 
     @classmethod
-    def detect_checkers(cls, board, frame, bgr_red = [50,80,220], bgr_green = [205,110,60], color_dist_thresh = 7):
+    def detect_checkers(
+        cls,
+        board,
+        frame,
+        bgr_red=[50, 80, 220],
+        bgr_green=[205, 110, 60],
+        color_dist_thresh=7,
+    ):
 
         Checkers.checkers = []
 
-        for x in range(0,len(board.points)-1,1):
-            for y in range(0,len(board.points[x])-1,1):
+        for x in range(0, len(board.points) - 1, 1):
+            for y in range(0, len(board.points[x]) - 1, 1):
                 board_tile_pts = [
                     board.points[x][y],
-                    board.points[x][y+1],
-                    board.points[x+1][y+1],
-                    board.points[x+1][y]
+                    board.points[x][y + 1],
+                    board.points[x + 1][y + 1],
+                    board.points[x + 1][y],
                 ]
                 detected_checker_color = Checkers.detect_checker_color_if_present(
                     frame,
                     get_avg_pos(board_tile_pts),
-                    bgr_red, 
-                    bgr_green, 
-                    color_dist_thresh
+                    bgr_red,
+                    bgr_green,
+                    color_dist_thresh,
                 )
                 if detected_checker_color is not None:
-                    Checkers.checkers.append(Checkers(pos=[x,y],color=detected_checker_color))
-
-
+                    Checkers.checkers.append(
+                        Checkers(pos=[x, y], color=detected_checker_color)
+                    )
 
     @classmethod
-    def detect_checker_color_if_present(cls, img, pt, bgr_red, bgr_green, color_dist_thresh, radius = 2):
-        test_sample = img[(pt[1]-radius):(pt[1]+radius), (pt[0]-radius):(pt[0]+radius)]
+    def detect_checker_color_if_present(
+        cls, img, pt, bgr_red, bgr_green, color_dist_thresh, radius=2
+    ):
+        test_sample = img[
+            (pt[1] - radius) : (pt[1] + radius), (pt[0] - radius) : (pt[0] + radius)
+        ]
         bgr_sample_value = get_avg_color(test_sample)
-        if distance_from_color(bgr_sample_value, bgr_red)<=color_dist_thresh:
+        if distance_from_color(bgr_sample_value, bgr_red) <= color_dist_thresh:
             return Color.RED
-        if distance_from_color(bgr_sample_value, bgr_green) <=color_dist_thresh:
+        if distance_from_color(bgr_sample_value, bgr_green) <= color_dist_thresh:
             return Color.GREEN
         return None
 
-
-    def __init__(self, pos = [0,0], color = Color.GREEN):
+    def __init__(self, pos=[0, 0], color=Color.GREEN):
         self.pos = pos
         self.color = color
 
     # currently not used
-    def classify_color_by_hue(self, hue_val = 0):
+    def classify_color_by_hue(self, hue_val=0):
 
         green_dist_1 = abs(hue_val - Checkers.hue_val_green)
         green_dist_2 = 180 - abs(hue_val - Checkers.hue_val_green)
@@ -129,4 +145,4 @@ class Checkers:
             self.color = Color.GREEN
         else:
             self.color = Color.RED
-        #print(f'My hue is {hue_val}\nMy {green_dist = }\nMy {red_dist = }\nMy color is {self.color.name}')
+        # print(f'My hue is {hue_val}\nMy {green_dist = }\nMy {red_dist = }\nMy color is {self.color.name}')
--- robot_manipulation/calibrate_to_file.py
+++ robot_manipulation/calibrate_to_file.py
@@ -7,26 +7,16 @@ from os.path import exists
 import os
 
 
-class DobotController():
-
-    @classmethod
-    def get_xy_from_id(cls, field_id: int):
-        y = (field_id - 1) // 4
-
-        x = ((field_id - 1) % 4) * 2
-        if y % 2 == 0:
-            x += 1
-
-        return x, y
+class DobotController:
 
     def __init__(self, default: np.ndarray):
 
         # Connecting to DOBOT
         available_ports = list_ports.comports()
 
-        print('\nPlease select port by index')
+        print("\nPlease select port by index")
         for i, p in enumerate(available_ports):
-            print(f'[{i}]: {p}')
+            print(f"[{i}]: {p}")
 
         port_idx = int(input())
 
@@ -35,7 +25,7 @@ class DobotController():
         self.device = Dobot(port=port)
 
         x, y, z, _ = self.device.get_pose().position
-        print(f'\nx:{x} y:{y} z:{z}')
+        print(f"\nx:{x} y:{y} z:{z}")
 
         self.positions = np.zeros((42, 3), dtype=float)
 
@@ -48,24 +38,24 @@ class DobotController():
         for i in range(0, 32, 1):
             self.go_to_next_pos_from_file(height, i)
 
-            print('\nSet to position of id ' + str(i + 1))
+            print("\nSet to position of id " + str(i + 1))
             self.set_ith_position_manually(i)
 
         for i in range(32, 36, 1):
             self.go_to_next_pos_from_file(i)
 
-            print('\nSet to side pocket (left) of id ' + str(i - 31))
+            print("\nSet to side pocket (left) of id " + str(i - 31))
             self.set_ith_position_manually(i)
 
         for i in range(36, 40, 1):
             self.go_to_next_pos_from_file(i)
 
-            print('\nSet to side pocket (right) of id ' + str(i - 35))
+            print("\nSet to side pocket (right) of id " + str(i - 35))
             self.set_ith_position_manually(i)
 
         self.go_to_next_pos_from_file(height, 40)
 
-        print('\nSet to dispose area')
+        print("\nSet to dispose area")
         self.keyboard_move_dobot()
         x, y, z, _ = self.device.get_pose().position
         self.positions[40][0] = x
@@ -75,7 +65,7 @@ class DobotController():
 
         self.go_to_next_pos_from_file(height, 41)
 
-        print('\nSet to home position')
+        print("\nSet to home position")
         self.keyboard_move_dobot()
         x, y, z, _ = self.device.get_pose().position
         self.positions[41][0] = x
@@ -83,12 +73,19 @@ class DobotController():
         self.positions[41][2] = z
         self.move_arm(x, y, z + height, True)
 
-        print('\nCalibration done\n')
+        print("\nCalibration done\n")
 
     def go_to_next_pos_from_file(self, i: int, height: int = 10):
         if self.default is not None:
-            self.move_arm(self.default[i][0], self.default[i][1], self.default[i][2] + height, True)
-            self.move_arm(self.default[i][0], self.default[i][1], self.default[i][2], True)
+            self.move_arm(
+                self.default[i][0],
+                self.default[i][1],
+                self.default[i][2] + height,
+                True,
+            )
+            self.move_arm(
+                self.default[i][0], self.default[i][1], self.default[i][2], True
+            )
 
     def set_ith_position_manually(self, i: int, height: int = 10):
         self.keyboard_move_dobot()
@@ -101,7 +98,9 @@ class DobotController():
     def keyboard_move_dobot(self, increment: float = 1.0):
         x, y, z, _ = self.device.get_pose().position
 
-        instruction_frame = np.zeros(shape=(300, 300))  # TODO - instruction how to use frame
+        instruction_frame = np.zeros(
+            shape=(300, 300)
+        )  # TODO - instruction how to use frame
 
         while True:
             self.move_arm(x, y, z, wait=True)
@@ -114,20 +113,20 @@ class DobotController():
 
             if key == 13:
                 break
-            elif key == ord('w'):
+            elif key == ord("w"):
                 # y += increment
                 x += increment
-            elif key == ord('s'):
+            elif key == ord("s"):
                 # y -= increment
                 x -= increment
-            elif key == ord('a'):
+            elif key == ord("a"):
                 # x -= increment
                 y += increment
-            elif key == ord('d'):
+            elif key == ord("d"):
                 y -= increment
-            elif key == ord('q'):
+            elif key == ord("q"):
                 z -= increment
-            elif key == ord('e'):
+            elif key == ord("e"):
                 z += increment
 
         cv.destroyAllWindows()
@@ -144,20 +143,20 @@ if __name__ == "__main__":
     print("\nWould you like to use base configuration file? [y]/[n]")
     while True:
         user_input = input()
-        if user_input == 'y':
+        if user_input == "y":
             use_file = True
             break
-        if user_input == 'n':
+        if user_input == "n":
             use_file = False
             break
         print("Wrong input")
 
     if use_file:
-        if exists('robot_manipulation/configuration_files'):
-            configs = os.listdir('robot_manipulation/configuration_files')
+        if exists("robot_manipulation/configuration_files"):
+            configs = os.listdir("robot_manipulation/configuration_files")
             print("\nPlease choose a file by id\n")
             for id, c in enumerate(configs):
-                print(f'[{id}]: {c}')
+                print(f"[{id}]: {c}")
 
             while True:
                 user_input = int(input())
@@ -165,7 +164,7 @@ if __name__ == "__main__":
                     base_file = configs[user_input]
                     break
 
-            f = open("robot_manipulation/configuration_files/" + base_file, 'r')
+            f = open("robot_manipulation/configuration_files/" + base_file, "r")
             lines = f.readlines()
             f.close()
             if len(lines) < 42:
@@ -173,7 +172,7 @@ if __name__ == "__main__":
 
             result_list: np.ndarray = np.zeros((42, 3), dtype=float)
             for i in range(0, 42, 1):
-                pos_xyz = lines[i].split(';')
+                pos_xyz = lines[i].split(";")
                 result_list[i][0] = float(pos_xyz[0])
                 result_list[i][1] = float(pos_xyz[1])
                 result_list[i][2] = float(pos_xyz[2])
@@ -186,7 +185,7 @@ if __name__ == "__main__":
     else:
         dobot = DobotController(None)
 
-    print('\nPut name of the file you would like to save configuration in:')
+    print("\nPut name of the file you would like to save configuration in:")
 
     filename = input()
 
@@ -194,10 +193,13 @@ if __name__ == "__main__":
 
     for i in range(0, 42, 1):
         f.write(
-            str(dobot.positions[i][0]) + ';' +
-            str(dobot.positions[i][1]) + ';' +
-            str(dobot.positions[i][2]) + '\n'
+            str(dobot.positions[i][0])
+            + ";"
+            + str(dobot.positions[i][1])
+            + ";"
+            + str(dobot.positions[i][2])
+            + "\n"
         )
 
     f.close()
-    print('\nDONE!\n')
+    print("\nDONE!\n")
