1d39778e0793d388b3a95c0012b5d1c70cf82f65
OlekZima <zimenkoalex34@gmail.com>
2024-11-25 19:20:10 +0100

    Refactor utilities
    Make list_camera_ports shorten, add distance_from_color fun

--- src/checkers_game_and_decissions/utilities.py
+++ src/checkers_game_and_decissions/utilities.py
@@ -8,7 +8,9 @@ import cv2
 from src.checkers_game_and_decissions.enum_entities import Color
 
 
-def get_coord_from_field_id(field_id: int, color: Optional[Color] = None) -> tuple[int, int]:
+def get_coord_from_field_id(
+    field_id: int, color: Optional[Color] = None
+) -> tuple[int, int]:
     y, x = divmod((field_id - 1), 4)
     x *= 2
 
@@ -37,7 +39,7 @@ def linear_interpolate(a: float, b: float, t: float) -> float:
     return a + t * (b - a)
 
 
-def flush_input():
+def flush_input() -> None:
     termios.tcflush(sys.stdin, termios.TCIOFLUSH)
 
 
@@ -46,50 +48,17 @@ def get_avg_color(img):
     return avg_color.astype(int).tolist()
 
 
-# def get_board_mask(pts, img_shape, margin=10):
-#     center = get_avg_pos(pts)
-#
-#     for pt in pts:
-#         dst = get_pts_dist(pt, center)
-#         pt[0] = center[0] + int(float(pt[0] - center[0]) / dst * (dst + margin))
-#         pt[1] = center[1] + int(float(pt[1] - center[1]) / dst * (dst + margin))
-#
-#     pts = np.array(pts)
-#     mask = np.zeros(img_shape[:2], dtype="uint8")
-#     cv2.fillConvexPoly(mask, pts, 1)
-#     return mask
-
-
 def list_camera_ports() -> list[int]:
-    # Test the ports and returns a tuple with the available ports and the ones that are working.
-
-    # is_working = True
-    dev_port = 0
     working_ports = []
-    # available_ports = []
-    while dev_port < 10:  # is_working:
-        camera = cv2.VideoCapture(dev_port)
-        if not camera.isOpened():
-            # is_working = False
-            print("Port %s is not working." % dev_port)
-            dev_port += 1
-        else:
-            is_reading, img = camera.read()
-            w = camera.get(3)
-            h = camera.get(4)
-            if is_reading:
-                print(
-                    "Port %s is working and reads images (%s x %s)" % (dev_port, h, w)
-                )
-                working_ports.append(dev_port)
-            # else:
-            #     print(
-            #         "Port %s for camera ( %s x %s) is present but does not reads."
-            #         % (dev_port, h, w)
-            #     )
-                # available_ports.append(dev_port)
-            dev_port += 1
-    return working_ports  # available_ports, working_ports
+    for dev_port in range(10):
+        cap = cv2.VideoCapture(dev_port)
+        is_reading, img = cap.read()
+        h, w, _ = img.shape
+        if is_reading:
+            print(f"Port {dev_port} is working and reads image ({h} x {w})")
+            working_ports.append(dev_port)
+        cap.release()
+    return working_ports
 
 
 def get_pts_dist(pt1, pt2):
@@ -109,5 +78,10 @@ def get_avg_pos(pts=None):
     return [x_avg, y_avg]
 
 
-def empt_fun(a):
+def distance_from_color(bgr_sample, bgr_target) -> float:
+    distance = sum([(bgr_sample[i] - bgr_target[i]) ** 2 for i in range(3)])
+    return math.sqrt(distance)
+
+
+def empty_function(a):
     pass
