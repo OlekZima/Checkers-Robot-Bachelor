d83f3f17de1f3671b8a8977079182fdc58469d6c
OlekZima <zimenkoalex34@gmail.com>
2024-11-25 21:20:57 +0100

    Refactor to make code more type strict

--- src/computer_vision/checkers_recognition.py
+++ src/computer_vision/checkers_recognition.py
@@ -1,32 +1,32 @@
 import math
-from typing import Optional
+from typing import  Optional, Self
 from src.checkers_game_and_decisions.utilities import get_avg_color, get_avg_pos
 from src.checkers_game_and_decisions.enum_entities import Color
 
 
 class Checkers:
-    def __init__(self, pos: Optional[list[int]] = None, color=Color.BLUE):
-        self.pos = pos if pos is not None else [0, 0]
-        self.color = color
+    checkers: list[Self] = []
+    bgr_blue: list[int] = [205, 110, 60]
+    bgr_orange: list[int] = [50, 80, 220]
+    color_dist_thresh: int = 7
 
-    checkers = []
-    bgr_blue = [205, 110, 60]
-    bgr_orange = [50, 80, 220]
-    color_dist_thresh = 7
+    def __init__(self, color: Color, pos: Optional[tuple[int, int]] = None):
+        self.pos: tuple[int, int] = pos if pos is not None else (0, 0)
+        self.color = color
 
     @classmethod
     def detect_checkers(cls, board, frame):
         cls.checkers = []
 
-        for x in range(0, len(board.points) - 1, 1):
-            for y in range(0, len(board.points[x]) - 1, 1):
+        for x in range(len(board.points) - 1):
+            for y in range(len(board.points[x]) - 1):
                 board_tile_pts = cls.get_board_tile_pts(board, x, y)
                 detected_color = Checkers.detect_checker_color_if_present(
                     frame,
                     get_avg_pos(board_tile_pts),
                 )
                 if detected_color is not None:
-                    cls.checkers.append(Checkers([x, y], detected_color))
+                    cls.checkers.append(Checkers(detected_color, (x, y)))
 
     @staticmethod
     def get_board_tile_pts(board, x, y):
@@ -38,7 +38,7 @@ class Checkers:
         ]
 
     @classmethod
-    def detect_checker_color_if_present(cls, img, pt, radius=2) -> Optional[Color]:
+    def detect_checker_color_if_present(cls, img, pt:tuple[int, int], radius=2) -> Optional[Color]:
         test_sample = img[
             (pt[1] - radius) : (pt[1] + radius), (pt[0] - radius) : (pt[0] + radius)
         ]
