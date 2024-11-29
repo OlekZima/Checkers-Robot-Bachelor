e861c30df68868a8fd83f460f838ae85ebae41c4
OlekZima <zimenkoalex34@gmail.com>
2024-11-30 00:32:58 +0100

    Uncomment ConfigurationWindow to test position configuration to the window

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from src.GUI.init_window import ConfigurationWindow
 
 
 def main() -> None:
-    # config_window = ConfigurationWindow()
-    # config_window.run()
+    config_window = ConfigurationWindow()
+    config_window.run()
 
-    # print(
-    #     f"{config_window.get_camera_port()=}\n{config_window.get_robot_port()=}\n{config_window.get_config_colors_dict()=}"
-    # )
+    print(
+        f"{config_window.get_camera_port()=}\n{config_window.get_robot_port()=}\n{config_window.get_config_colors_dict()=}"
+    )
 
-    game_window = GameWindow("", "")
-    game_window.run()
+    # camera_port = config_window.get_camera_port()
+    # game_window = GameWindow("", "", )
+    # game_window.run()
 
 if __name__ == "__main__":
     main()
