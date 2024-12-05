385b0a0ad0522302de5e193e73b180a7407d1cc1
OlekZima <zimenkoalex34@gmail.com>
2024-12-05 13:53:10 +0100

    Testing game window GUI

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+from src.GUI import game_window
 from src.GUI.game_window import GameWindow
 from src.GUI.init_window import ConfigurationWindow
 
 
 def main() -> None:
-    config_window = ConfigurationWindow()
-    config_window.run()
+    # config_window = ConfigurationWindow()
+    # config_window.run()
 
-    print(
-        f"{config_window.get_camera_port()=}\n{config_window.get_robot_port()=}\n{config_window.get_config_colors_dict()=}"
-    )
+    # print(
+    #     f"{config_window.get_camera_port()=}\n{config_window.get_robot_port()=}\n{config_window.get_config_colors_dict()=}"
+    # )
 
+    # robot_color = config_window.get_robot_color()
+    # robot_port = config_window.get_robot_port()
     # camera_port = config_window.get_camera_port()
-    # game_window = GameWindow("", "", )
-    # game_window.run()
+    # color_config = config_window.get_config_colors_dict()
+
+    # game_window = GameWindow(robot_color, robot_port, camera_port, color_config)
+    game_window = GameWindow("", "", 2)
+    game_window.run()
+
 
 if __name__ == "__main__":
     main()
