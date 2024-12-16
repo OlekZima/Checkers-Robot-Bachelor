f16aed88c815ae9fa8ba55d31496e56f7a2e44ca
OlekZima <zimenkoalex34@gmail.com>
2024-12-16 15:32:07 +0100

    Create new dataclass for most of the parameters

--- src/common/dataclasses.py
+++ src/common/dataclasses.py
@@ -5,18 +5,16 @@ from typing import Tuple
 
 
 @dataclass
-class ContourParams:
-    """Helper class for storing parameters for the ContourProcessor class.
+class ColorConfig:
+    orange: Tuple[int, int, int]
+    blue: Tuple[int, int, int]
+    black: Tuple[int, int, int]
+    light: Tuple[int, int, int]
 
-    Attributes:
-        min_area (int): minimal area of the contour
-        area_margin (int): margin for the area of the contour
-        approx_peri_fraction (float): fraction of the perimeter for the approxPolyDP method
-        px_dist_to_join (float): distance between points to join them
-        threshold1 (int): first threshold for the Canny method
-        threshold2 (int): second threshold for the Canny method
-        kernel_size (Tuple[int, int]): size of the kernel for the dilate method
-    """
+
+@dataclass
+class RecognitionConfig:
+    """Configuration for checking color o tile 00."""
 
     min_area: int = 150
     area_margin_percent: int = 20
@@ -25,15 +23,6 @@ class ContourParams:
     threshold1: int = 140
     threshold2: int = 255
     kernel_size: Tuple[int, int] = (2, 2)
-
-
-@dataclass
-class RecognitionConfig:
-    """Configuration for checking color o tile 00."""
+    color_dist_threshold: int = 60
 
     radius: int = 4
-    dark_field_bgr: Tuple[int] = (0, 0, 0)
-    light_field_bgr: Tuple[int] = (255, 255, 255)
-    orange_bgr: Tuple[int] = (0, 0, 255)
-    blue_bgr: Tuple[int] = (0, 255, 0)
-    color_dist_thresh: int = 60
